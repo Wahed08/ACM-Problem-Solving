e9d5421513aedef19fa678607fd05341e66acb0a
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-05-28 14:57:23 +0600

    Create Lucky Numbers.cpp

---
+++ Lucky Numbers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   int n;
+   cin>>n;
+
+   long long int k=pow(2,n+1)-2;
+   cout<<k<<endl;
+
+ return 0;
+}
