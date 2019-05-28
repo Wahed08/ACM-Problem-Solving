bc0a1dd934e35c0f456baf5287a3b725f79953e9
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-05-28 14:56:50 +0600

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    long long int n;
+    cin>>n;
+
+    cout<<2-(n%2)<<endl;
+
+    return 0;
+}
