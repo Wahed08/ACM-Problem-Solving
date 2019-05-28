3d4cf461f6a0b8491884a1925c41bebc71ca89c6
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-05-28 14:58:04 +0600

    Create Hexagones!.cpp

---
+++ Hexagones!.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    long long int n,k;
+    cin>>n;
+    k=1+3*n*(n+1);
+
+    cout<<k<<endl;
+
+ return 0;
+}
