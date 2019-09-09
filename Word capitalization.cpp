1463e8559e4549896245c1db86fa090e9a06be05
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-09-10 02:42:19 +0600

    Create Word capitalization.cpp

---
+++ Word capitalization.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+    string str;
+    cin>>str;
+ 
+ 
+        if(str[0]>=97 && str[0]<=122)
+        {
+            str[0]=str[0]-32;
+        }
+ 
+    cout<<str<<endl;
+ 
+ 
+    return 0;
+}
