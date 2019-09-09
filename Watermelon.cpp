a2265c360f9dfa32688a44b8b1578acfbeb05bd3
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-09-10 02:39:14 +0600

    Create Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int w,i;
+ 
+    scanf("%d",&w);
+ 
+    if(w%2==0 && w>2)
+        printf("YES\n");
+ 
+    else
+        printf("NO\n");
+ 
+    return 0;
+}
