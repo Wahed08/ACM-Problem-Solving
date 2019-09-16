b020f9dfd53b7a40fc0aaae40a87799c5e2ec244
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-09-16 22:49:56 +0600

    Create Game with Sticks.cpp

---
+++ Game with Sticks.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+ 
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+ 
+    if(min(n,m)%2!=0)
+        cout<<"Akshat"<<endl;
+    else
+        cout<<"Malvika"<<endl;
+ 
+    return 0;
+}
