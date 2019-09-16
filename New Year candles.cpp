74f969b1cd73cfde02f5fe459d8945f1c01542c6
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-09-16 22:45:29 +0600

    Create New Year candles.cpp

---
+++ New Year candles.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+    int a,b,x;
+ 
+    cin>>a>>b;
+ 
+   x=(a-1)/(b-1);
+   ceil(x);
+ 
+    cout<< a+x <<endl;
+    return 0;
+}
