58bbcfa7b4565e2eb7434038cfb3ad44f353594d
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-09-16 22:45:56 +0600

    Create vasya and socks.cpp

---
+++ vasya and socks.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+    int n,m,x;
+    cin>>n>>m;
+ 
+    x=(n-1)/(m-1);
+    ceil(x);
+    cout<<n+x<<endl;
+ 
+ return 0;
+}
