6fd47310b801c1339855b229d046fb4e5fc31166
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-10-01 18:29:45 +0600

    Create Arpa’s hard exam and Mehrdad’s naive cheat.cpp

---
+++ Arpa’s hard exam and Mehrdad’s naive cheat.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long int n;
+    cin>>n;
+ 
+    if(n==0)
+    {
+        cout<<"1"<<endl;
+        return 0;
+    }
+    else if(n%4==0)
+        cout<<"6"<<endl;
+    else if(n%4==1)
+        cout<<"8"<<endl;
+    else if(n%4==2)
+        cout<<"4"<<endl;
+    else if(n%4==3)
+        cout<<"2"<<endl;
+ 
+    return 0;
+}
