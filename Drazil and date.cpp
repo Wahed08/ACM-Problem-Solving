b5ad4b01f8794f2267d5afaf80e4171e30b42f9e
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-10-03 19:24:12 +0600

    Create Drazil and date.cpp

---
+++ Drazil and date.cpp
@@ -0,0 +1,31 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long int a,b,s;
+    cin>>a>>b>>s;
+ 
+     if(a==0 and b==0 and s==2)
+    {
+        cout<<"Yes"<<endl;
+        return 0;
+    }
+    if(abs(a)+abs(b)>s){
+        cout<<"No"<<endl;
+        return 0;
+    }
+ 
+    if(abs(a)+abs(b)==s || (s-(abs(a))+abs(b))%2==0)
+    {
+        cout<<"Yes"<<endl;
+        return 0;
+    }
+    else{
+        cout<<"No"<<endl;
+        return 0;
+    }
+ 
+    return 0;
+}
