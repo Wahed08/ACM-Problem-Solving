b6477dda4f7b10d55ddcd4e540e2f2ed9e8ad7a7
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-10-16 04:07:32 +0600

    Create lovely palindromes.cpp

---
+++ lovely palindromes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    string str;
+    cin>>str;
+    cout<<str;
+    reverse(str.begin(),str.end());
+    cout<<str;
+    cout<<endl;
+ 
+    return 0;
+}
