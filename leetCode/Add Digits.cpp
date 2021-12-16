5a950c18688906e81e7c814551fdc1ae33d805f9
Mohammad Farhad <wahedfarhad123@gmail.com>
2021-12-16 13:30:38 +0600

    Create Add Digits.cpp

---
+++ leetCode/Add Digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        if(num == 0)
+            return 0;
+        else
+        {
+            return 1+(num-1)%(10-1);
+        }
+    }
+};
