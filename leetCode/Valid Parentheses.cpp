d9e52ad5bc954eec5e51d8e5238a3d8875f2a42f
Mohammad Farhad <wahedfarhad123@gmail.com>
2021-12-19 20:17:31 +0600

    Create Valid Parentheses.cpp

---
+++ leetCode/Valid Parentheses.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        char c;
+        
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+                continue;
+            }
+            if(st.empty())
+                return false;
+            
+            switch(s[i]){
+                case ')':
+                    c = st.top();
+                    st.pop();
+                    if(c == '{' || c == '[')
+                        return false;
+                    break;
+                    
+                    case '}':
+                    c = st.top();
+                    st.pop();
+                    if(c == '(' || c == '[')
+                        return false;
+                    break;
+                    
+                    case ']':
+                    c = st.top();
+                    st.pop();
+                    if(c == '{' || c == '(')
+                        return false;
+                    break;
+                    
+            }
+        }
+        return st.empty();
+    }
+};
