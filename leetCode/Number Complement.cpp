5996b2ef22ffd613c948cb55f33beb8829bc8b71
Mohammad Farhad <wahedfarhad123@gmail.com>
2021-12-27 13:47:19 +0600

    Update Number Complement.cpp

--- leetCode/Number Complement.cpp
+++ leetCode/Number Complement.cpp
@@ -15,13 +15,15 @@ public:
         
         for(int i=0; i<vec.size(); i++){
            if(vec[i] == 0)
-               str +='1';
-            else str +='0';
+               vec[i] = 1;
+            else vec[i] = 0;
         }
-        // for(int i=0; i<vec.size(); i++){
-        //     cout<<vec[i]<<" ";
-        // }
         
-        return stoi(str, 0, 2);;
+        int res = 0;
+        for(int i=0; i<vec.size(); i++){
+            res = res*2 + vec[i];
+        }
+        
+        return res;
     }
 };
