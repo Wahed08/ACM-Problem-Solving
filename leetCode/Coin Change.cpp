d8cc88d5de18da6f52289badd32dfd5dd64ef5eb
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-01-29 15:58:23 +0600

    Create Coin Change.cpp

---
+++ leetCode/Coin Change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int length = coins.size();
+        
+        vector<long>one(amount+1, INT_MAX);
+        one[0] = 0;
+        // vector<long>two(amount+1, -1); for find the which coins make amount
+        
+        for(int i=0; i<length; i++){
+            for(int j = 0; j <= amount; j++){
+                if(j >= coins[i]){
+                    one[j] = min(one[j], 1 + one[j-coins[i]]);
+                    // two[j] = i;
+                }
+            }
+        }
+        if(one[amount] == INT_MAX)
+            return -1;
+        return one[amount];
+    }
+};
