11c4f2382d8dda2060c84faf8effca5f919f3ab6
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-02-08 21:42:40 +0600

    Update single element in a sorted array.cpp

--- leetCode/single element in a sorted array.cpp
+++ leetCode/single element in a sorted array.cpp
@@ -15,3 +15,17 @@ public:
         return k;
     }
 };
+
+// using xor operation
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int res = 0, i;
+        
+        for (i = 0; i < nums.size(); i++)
+            res ^= nums[i];
+        
+         return res;
+    }
+};
