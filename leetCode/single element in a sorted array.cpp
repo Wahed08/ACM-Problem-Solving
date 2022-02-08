37763daef61589234a2756e85b6924c0e03b5e99
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-02-08 21:47:17 +0600

    Update single element in a sorted array.cpp

--- leetCode/single element in a sorted array.cpp
+++ leetCode/single element in a sorted array.cpp
@@ -29,3 +29,28 @@ public:
          return res;
     }
 };
+
+//using binary search(logn) approach
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+         int left = 0, right = nums.size() - 2;
+        
+        while(left <= right){
+            int mid = left + (right-left) / 2;
+            if(mid % 2 == 0){
+                if(nums[mid] == nums[mid+1])
+                    left = mid+1;
+                else
+                    right = mid -1;
+            }else if(mid % 2 != 0){
+                if(nums[mid] == nums[mid-1])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return nums[left];
+    }
+};
