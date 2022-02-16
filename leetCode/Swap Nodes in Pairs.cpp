ce282d97d1585368c0130afdd40e419604180011
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-02-16 13:18:48 +0600

    Create Swap Nodes in Pairs.cpp

---
+++ leetCode/Swap Nodes in Pairs.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        if(head == NULL || head->next == NULL)
+            return head;
+        
+        ListNode* temp = head;
+        ListNode* prev = NULL, *result, *t;
+        queue<ListNode*>q;
+        
+        while(temp != NULL){
+            q.push(temp);
+            temp = temp->next;
+        }
+        int flag = 0;
+        ListNode* curr, *pr;
+        while(!q.empty()){
+            
+            if(q.size() == 1){
+                result->next = q.front();
+                return prev;
+            }
+            
+            curr = q.front();
+            q.pop();
+            pr = q.front();
+            q.pop();
+            flag++;
+            
+            pr->next = curr;
+            
+            curr->next = NULL;
+            if(flag == 1){
+                prev = pr;
+                result = pr->next;
+            }
+            if(flag != 1){
+                result->next = pr;
+                result = result->next->next;
+            }
+           
+        }
+        
+        return prev;
+    }
+};
