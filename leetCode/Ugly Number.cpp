760490c3b1d9dda08e9a6d28d787edf0723f1d6b
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-02-23 19:34:29 +0600

    Create Ugly Number.cpp

---
+++ leetCode/Ugly Number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        if(n == 0)
+            return false;
+        
+        while(n != 1){
+            if(n % 2 == 0)
+                n /= 2;
+            else if(n % 3 == 0)
+                n /= 3;
+            else if(n % 5 == 0)
+                n /= 5;
+            else
+                return false;
+            
+        }
+        return true;
+    }
+};
