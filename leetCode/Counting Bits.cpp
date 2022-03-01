06d86378972f3a976eae0bf595bc122ccd95060c
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-03-01 13:46:42 +0600

    Update Counting Bits.cpp

--- leetCode/Counting Bits.cpp
+++ leetCode/Counting Bits.cpp
@@ -1,3 +1,5 @@
+// O(nlogn) solution
+
 class Solution {
 public:
     vector<int> countBits(int n) {
@@ -15,6 +17,25 @@ public:
                 x /= 2;
             }
             vec.push_back(count);
+        }
+        
+        return vec;
+    }
+};
+
+// O(n) solution--using DP
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        vector<int>vec(n+1, 0);
+        
+        for(int i=1; i<=n; i++){
+            vec[i] = vec[i/2] + i % 2; 
         }
         
         return vec;
