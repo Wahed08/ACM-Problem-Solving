7cd8c96bf24a037ee05e9e27bbbfa2cec56e7e57
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-03-03 13:05:26 +0600

    Update Search a 2D Matrix II.cpp

--- leetCode/Search a 2D Matrix II.cpp
+++ leetCode/Search a 2D Matrix II.cpp
@@ -23,3 +23,32 @@ public:
         return false;
     }
 };
+
+
+// more efficient solution
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int row = 0, col = n-1;
+        
+        while(row < m and col >=0){
+            int value = matrix[row][col];
+            if(value == target)
+                return true;
+            else if(value < target)
+                row++;
+            else
+                col--;
+        }
+        
+        return false;
+    }
+};
