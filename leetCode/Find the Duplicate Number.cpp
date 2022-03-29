d2d691c6a7419b07a26d3e351143c35bf1b2fbf7
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-03-29 19:27:24 +0600

    Update Find the Duplicate Number.cpp

--- leetCode/Find the Duplicate Number.cpp
+++ leetCode/Find the Duplicate Number.cpp
@@ -25,10 +25,36 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(0), cout.tie(0);
         
-        //interesting
+        //interesting -> this is called tortoise and hare algorithim
         while(nums[0] != nums[nums[0]])
             swap(nums[0], nums[nums[0]]);
         
         return nums[0];
+    }
+};
+
+// full code of tortoise and hare algo ->
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int slow = nums[0], fast = nums[0];
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        while(slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
     }
 };
