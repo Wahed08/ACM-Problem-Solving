d96042618076732c26ac05239f88fdb837fbb53b
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-04-09 16:22:05 +0600

    Update Top K Frequent Elements.cpp

--- leetCode/Top K Frequent Elements.cpp
+++ leetCode/Top K Frequent Elements.cpp
@@ -29,3 +29,33 @@ public:
         return ans;
     }
 };
+
+// priority_queue solution
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int n = nums.size();
+        map<int, int>mapp;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq; //min-heap
+        vector<int>ans;
+        
+        for(int i=0; i<n; i++) mapp[nums[i]]++;
+        for(auto it : mapp){
+            pq.push({it.second, it.first});
+            if(pq.size() > k)
+                pq.pop();
+        }
+        
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        return ans;
+    }
+};
