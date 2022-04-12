151c415600f350db34ac37b37bdc689edd29935e
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-04-12 15:38:54 +0600

    Update Game of Life.cpp

--- leetCode/Game of Life.cpp
+++ leetCode/Game of Life.cpp
@@ -51,3 +51,46 @@ public:
         }
     }
 };
+
+// without creating linear vector
+class Solution {
+public:
+    int m, n;
+    int find(vector<vector<int>>& board, int i, int j, int sum){
+        
+         i-1 >= 0 and j-1 >= 0 and board[i-1][j-1]==1?sum++:sum;
+         i-1 >= 0 and board[i-1][j]==1?sum++:sum;
+         i-1 >= 0 and j+1 < n and board[i-1][j+1]==1?sum++:sum;
+         j-1 >= 0 and board[i][j-1]==1?sum++:sum;
+         j+1 < n and board[i][j+1]==1?sum++:sum;
+         i+1 < m and j-1 >= 0 and board[i+1][j-1]==1?sum++:sum;
+         i+1 < m and board[i+1][j]==1?sum++:sum;
+         i+1 < m and j+1 < n and board[i+1][j+1]==1?sum++:sum;
+        
+        return sum;
+    }
+    
+    void gameOfLife(vector<vector<int>>& board) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        m = board.size();
+        n = board[0].size();
+        vector<vector<int>>vec = board;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int sum = 0;
+                if(board[i][j]==1){
+                    int val = find(vec, i, j, sum);
+                    board[i][j] = (val == 2 || val == 3)? 1:0;
+                }
+                else{
+                    int val = find(vec, i, j, sum);
+                    board[i][j] = (val==3)? 1:0;
+                }
+            }
+        }
+    }
+};
