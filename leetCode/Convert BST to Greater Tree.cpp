4c3e29366ec3a752371c448c237022f0e8dee2da
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-04-16 15:01:41 +0600

    Update Convert BST to Greater Tree.cpp

--- leetCode/Convert BST to Greater Tree.cpp
+++ leetCode/Convert BST to Greater Tree.cpp
@@ -47,3 +47,38 @@ public:
         return root;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    void updateTree(TreeNode* root){
+        if(root==NULL)
+            return;
+        
+        updateTree(root->right);
+        sum += root->val;
+        root->val = sum;
+        updateTree(root->left);
+    }
+    
+    TreeNode* convertBST(TreeNode* root) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        updateTree(root);
+        return root;
+    }
+};
