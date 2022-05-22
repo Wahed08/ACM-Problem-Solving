4dde0b073bfeebcd68b58479c7f6fdf29919e410
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-05-22 13:26:11 +0600

    Update Palindromic Substrings.cpp

--- leetCode/Palindromic Substrings.cpp
+++ leetCode/Palindromic Substrings.cpp
@@ -15,7 +15,6 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(0), cout.tie(0);
         
-        vector<string>str;
         int cnt=0;
         
         for(int i=0; i<s.length(); i++){
@@ -25,6 +24,40 @@ public:
                 if(checkPalindrome(x)){
                     cnt++;
                 }
+            }
+        }
+        
+        return cnt;
+    }
+};
+
+// another good way
+
+class Solution {
+public:
+    
+    int countSubstrings(string s) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int cnt = 0, n = s.length();
+        
+        for(int i=0; i<n; i++){
+            int left = i, right = i;
+            //for odd length check
+            while(left >= 0 and right < n and s[left] == s[right]){
+                cnt++;
+                left--; 
+                right++;
+            }
+            
+            left = i, right = i+1;
+            //for even length check
+            while(left >= 0 and right < n and s[left] == s[right]){
+                cnt++;
+                left--;
+                right++;
             }
         }
         
