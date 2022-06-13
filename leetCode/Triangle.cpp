8f332e5e4af212d3f03f4bf29c76ec79311cb808
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-06-13 19:16:49 +0600

    Update Triangle.cpp

--- leetCode/Triangle.cpp
+++ leetCode/Triangle.cpp
@@ -25,3 +25,31 @@ public:
         return sum;
     }
 };
+
+// DFS + dp(memoization)
+
+class Solution {
+public:
+    int dfs(vector<vector<int>>& triangle, int n, int i, int j, vector<vector<int>>&vec){
+        
+        if(i >= n)
+            return 0;
+        
+        if(vec[i][j] != -1)
+            return vec[i][j];
+        else {
+            return vec[i][j] = triangle[i][j] + min(dfs(triangle, n, i+1, j, vec), dfs(triangle, n, i+1, j+1, vec));
+        }
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int n = triangle.size();
+        vector<vector<int>>vec(n, vector<int>(n, -1));
+        
+        return dfs(triangle, n, 0, 0, vec);
+    }
+};
