72a88ff09c943a14ae76b979fcacb66278e8be79
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-07-01 18:26:10 +0600

    Update Maximum Subarray.cpp

--- leetCode/Maximum Subarray.cpp
+++ leetCode/Maximum Subarray.cpp
@@ -15,3 +15,25 @@ public:
         return maxSum;
     }
 };
+
+
+// dp memoization solution
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int cnt = INT_MIN;
+        vector<int>dp(nums.size(), 0);
+        dp[0] = nums[0];
+        
+        if(nums.size() == 1) return nums[0];
+        for(int i=1; i<nums.size(); i++)
+            dp[i] = max(nums[i], nums[i]+dp[i-1]);
+        
+        for(auto it : dp) cnt = max(cnt, it);
+        return cnt;
+    }
+};
