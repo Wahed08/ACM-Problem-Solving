117241feefff27b536a61c598237711b78650090
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-08-02 12:40:04 +0600

    Update Kth Smallest Element in a Sorted Matrix.cpp

--- leetCode/Kth Smallest Element in a Sorted Matrix.cpp
+++ leetCode/Kth Smallest Element in a Sorted Matrix.cpp
@@ -16,3 +16,29 @@ public:
         return vec[k-1];
     }
 };
+
+
+// O(1) space complexity
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int n = matrix.size();
+        int start = matrix[0][0], end = matrix[n-1][n-1];
+        
+        while(start < end){
+            int mid = start + (end-start)/2;
+            int count=0;
+            for(int i=0; i<n; i++)
+                count += upper_bound(matrix[i].begin(),matrix[i].end(),mid) - matrix[i].begin();
+
+            if(count < k) start = mid+1;
+            else end = mid;
+        }
+        return start;
+    }
+};
