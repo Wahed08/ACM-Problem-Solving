73356deff153250215ae0bec39b56fddd7fae4d2
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-08-05 15:44:55 +0600

    Update Combination Sum IV.cpp

--- leetCode/Combination Sum IV.cpp
+++ leetCode/Combination Sum IV.cpp
@@ -58,3 +58,32 @@ public:
         return combination(nums, target, dp);
     }
 };
+
+
+// More faster
+class Solution {
+public:
+    int combination(vector<int>&vec, int target, vector<int>&dp){
+        
+        if(target == 0) return 1;
+        if(target < 0) return 0;    
+        if(dp[target] != -1) return dp[target];
+        
+        int ans = 0;
+        for(int i=0; i<vec.size(); i++){
+            if(target >= vec[i]){
+                ans += combination(vec, target-vec[i], dp);
+            }
+        }
+        return dp[target] = ans;
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        vector<int>dp(target+1, -1);
+        return combination(nums, target, dp);
+    }
+};
