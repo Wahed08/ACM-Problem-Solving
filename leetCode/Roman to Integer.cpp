fa95974cb88cfcedb8309a0f2e9ce840231253d1
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-08-15 09:05:54 +0600

    Update Roman to Integer.cpp

--- leetCode/Roman to Integer.cpp
+++ leetCode/Roman to Integer.cpp
@@ -18,3 +18,23 @@ public:
         return ans;
     }
 };
+
+//
+class Solution {
+public:
+    int romanToInt(string s) {
+       
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        map<char, int>mp = {{'I',1},{'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000}};
+        int ans = 0, ln = s.length();
+        
+        for(int i=0; i<ln-1; i++){
+            if(mp[s[i]] >= mp[s[i+1]]) ans += mp[s[i]];
+            else ans -= mp[s[i]];
+        }
+        ans += mp[s[ln-1]];
+        return ans;
+    }
+};
