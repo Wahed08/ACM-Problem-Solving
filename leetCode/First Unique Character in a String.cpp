443fbb02c4c3b3672b4bde3be0c45c4385d97810
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-08-16 11:25:52 +0600

    Update First Unique Character in a String.cpp

--- leetCode/First Unique Character in a String.cpp
+++ leetCode/First Unique Character in a String.cpp
@@ -15,3 +15,22 @@ public:
         return -1;
     }
 };
+
+
+//more faster?
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        vector<int>mp(26, 0);
+        for(int i=0; i<s.length(); i++) mp[s[i] - 'a']++;
+        for(int i=0; i<s.length(); i++){
+            if(mp[s[i] - 'a'] == 1) return i;
+        }
+        return -1;
+    }
+};
