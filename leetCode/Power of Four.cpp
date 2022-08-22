516bc06ff7ddb44077a158f82e331c8bfc9b1f16
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-08-22 09:58:23 +0600

    Create Power of Four.cpp

---
+++ leetCode/Power of Four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+       
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        if(n <= 0) return false;
+        int32_t k = log(n) / log(4);
+        if(pow(4, k) == n) return true;
+        return false;
+    }
+};
