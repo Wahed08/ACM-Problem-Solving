95bf117242ca2afa8f9df7a5f266c86de1a7ae75
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-08-26 11:43:56 +0600

    Update Reordered Power of 2Reordered Power of 2.cpp

--- leetCode/Reordered Power of 2Reordered Power of 2.cpp
+++ leetCode/Reordered Power of 2Reordered Power of 2.cpp
@@ -24,3 +24,30 @@ public:
         return false;
     }
 };
+
+
+//
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        vector<string>vec;
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+        
+        do{
+            vec.push_back(s);
+        }while(next_permutation(s.begin(), s.end()));
+        
+        for(int i=0; i<vec.size(); i++){
+            
+            if(vec[i][0] == '0') continue;
+            int value = stoi(vec[i]);
+            if(log2(value) == (int)log2(value)) return true;
+        }
+        return false;
+    }
+};
