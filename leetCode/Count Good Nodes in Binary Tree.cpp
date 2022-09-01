8af24a8323b0b03e180deebbcd44c72791bc6406
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-09-01 15:52:06 +0600

    Update Count Good Nodes in Binary Tree.cpp

--- leetCode/Count Good Nodes in Binary Tree.cpp
+++ leetCode/Count Good Nodes in Binary Tree.cpp
@@ -9,7 +9,6 @@ public:
             sum++;
         }
         
-        
         int left = traversal(root->left, maximum);
         sum = 0;
         int right = traversal(root->right, maximum);
@@ -26,5 +25,29 @@ public:
         int ans2 = traversal(root->right, root->val);
         
         return ans1 + ans2 + 1;
+    }
+};
+
+//
+class Solution {
+public:
+    int traversal(TreeNode* root, int maximum){
+        
+        if(root == NULL) return 0;
+        int sum = 0;
+        if(root->val >= maximum){
+            maximum = max(root->val, maximum);
+            sum++;
+        }
+        return sum + traversal(root->left, maximum) + traversal(root->right, maximum);
+    }
+    
+    int goodNodes(TreeNode* root) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int ans = traversal(root, root->val);
+        return ans;
     }
 };
