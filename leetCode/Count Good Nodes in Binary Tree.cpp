4a8659b6147c915d1ce6bede87b1c52616399d27
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-09-01 15:45:56 +0600

    Create Count Good Nodes in Binary Tree.cpp

---
+++ leetCode/Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int sum = 0;
+    int traversal(TreeNode* root, int maximum){
+        
+        if(root == NULL) return sum;
+        if(root->val >= maximum){
+            maximum = max(root->val, maximum);
+            sum++;
+        }
+        
+        
+        int left = traversal(root->left, maximum);
+        sum = 0;
+        int right = traversal(root->right, maximum);
+        
+        return left + right;
+    }
+    
+    int goodNodes(TreeNode* root) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int ans1 = traversal(root->left, root->val);
+        int ans2 = traversal(root->right, root->val);
+        
+        return ans1 + ans2 + 1;
+    }
+};
