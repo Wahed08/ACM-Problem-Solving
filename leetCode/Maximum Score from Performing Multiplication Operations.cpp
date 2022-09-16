01aa1c036666aa7f93fe772929afca536f25c5b4
Mohammad Farhad <wahedfarhad123@gmail.com>
2022-09-16 12:43:00 +0600

    Update Maximum Score from Performing Multiplication Operations.cpp

--- leetCode/Maximum Score from Performing Multiplication Operations.cpp
+++ leetCode/Maximum Score from Performing Multiplication Operations.cpp
@@ -23,6 +23,34 @@ public:
     }
 };
 
+// Memory Limit Exceeded 43/50
+
+class Solution {
+public:
+    int check(vector<int>&nums,vector<int>&multipliers,int left,int right,int x,vector<vector<int>>&vec){
+        
+        if(x == multipliers.size()) return 0;
+        if(vec[left][right] != INT_MIN) return vec[left][right];
+        
+        int l = nums[left]*multipliers[x] + check(nums, multipliers, left+1, right, x+1, vec);
+        int r = nums[right]*multipliers[x] + check(nums, multipliers, left, right-1, x+1, vec);
+        
+        return vec[left][right] = max(l,r);
+    }
+    
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(0), cout.tie(0);
+        
+        int m = multipliers.size();
+        int n = nums.size();
+        vector<vector<int>>vec(m+1, vector<int>(n+1, INT_MIN));
+        
+        return check(nums, multipliers, 0, n-1, 0, vec);
+    }
+};
+
 
 // Accepted
 class Solution {
